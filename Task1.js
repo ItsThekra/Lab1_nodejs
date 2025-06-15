2f2f4125ea14111c43a694c5c15e8abcc01876e2
Thekra <thekra.aljagthmi@com>
2025-06-15 13:33:00 +0300

    first commit

--- Task1.js
+++ Task1.js
@@ -3,6 +3,19 @@ const http = require('http');
 // To serve the image in node.js: ( file system, readFile)
 const fs = require('fs');
 
+const html = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>node.js</title>
+</head>
+<body>
+    <h1>Hello Thekra</h1>
+</body>
+</html>`;
+
 const server = http.createServer((req, res) => {
 
     fs.readFile('image.jpg', (err, data) => {
@@ -12,11 +25,12 @@ const server = http.createServer((req, res) => {
       }
       res.writeHead(200, { 'Content-Type': 'image/jpeg' });
       res.end(data);
+    //   res.end(html)
     });
 
 });
 
 // give port to the server 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(3000, '127.0.0.1' , () => {
+  console.log('Server listening on port 127.0.0.1:3000');
 });
